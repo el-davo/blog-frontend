94391170f50d09d06b2dfeead285a105feeec713
ahernd2 <david.ahern26@gmail.com>
2017-03-24 16:34:53 +0000

    Reverting

--- app/common/base-url.ts
+++ app/common/base-url.ts
@@ -1,5 +1,5 @@
 export const baseUrl = {
-    development: 'https://prod-blog-backend.cfapps.io/api',
+    development: 'http://localhost:3001',
     production: 'https://prod-blog-backend.cfapps.io/api',
     dev: 'https://dev-blog-backend.cfapps.io/api',
     test: ''
