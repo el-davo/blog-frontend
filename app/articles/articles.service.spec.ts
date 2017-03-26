8eb4e26ad9ac4890106a36bafa29f2e963cedc90
ahernd2 <david.ahern26@gmail.com>
2017-03-26 14:15:22 +0100

    Removing huge json from lists

--- app/articles/articles.service.spec.ts
+++ app/articles/articles.service.spec.ts
@@ -1,261 +1,280 @@
-import { assert, spy } from 'sinon';
+import { spy } from 'sinon';
 import { load } from 'proxyquire';
-import { Article } from '../landing/landing.state';
 
 describe('Articles Service', () => {
 
-    const ARTICLES = [
-        {
-            id: 'a1',
-            name: 'Test 1 name',
-            content: 'Test 1 content'
-        },
-        {
-            id: 'a2',
-            name: 'Test 2 name',
-            content: 'Test 2 content'
+  const ARTICLES = [
+    {
+      id: 'a1',
+      name: 'Test 1 name',
+      content: 'Test 1 content'
+    },
+    {
+      id: 'a2',
+      name: 'Test 2 name',
+      content: 'Test 2 content'
+    }
+  ];
+
+  let jsonSpy;
+  let postSpy;
+  let putSpy;
+  let deleteSpy;
+  let articlesService;
+
+  describe('fetch articles', () => {
+
+    beforeEach(() => {
+      jsonSpy = spy();
+
+      class HttpService {
+        json(path: string) {
+          jsonSpy(path);
+          return Promise.resolve(ARTICLES);
         }
-    ];
+      }
 
-    let jsonSpy;
-    let postSpy;
-    let putSpy;
-    let deleteSpy;
-    let articlesService;
+      const {ArticlesService} = load('./articles.service', {
+        '../common/http.service': {
+          HttpService
+        }
+      });
 
-    describe('fetch articles', () => {
-
-        beforeEach(() => {
-            jsonSpy = spy();
-
-            class HttpService {
-                json(path: string) {
-                    jsonSpy(path);
-                    return Promise.resolve(ARTICLES);
-                }
-            }
-
-            const {ArticlesService} = load('./articles.service', {
-                '../common/http.service': {
-                    HttpService
-                }
-            });
-
-            articlesService = new ArticlesService();
-
-        });
-
-        it('should fetch a list of articles from the server', async () => {
-            const response = await articlesService.fetchArticles();
-
-            response.should.eql(ARTICLES);
-
-            jsonSpy.calledOnce.should.be.true();
-
-            jsonSpy.calledWith('/articles?filter={"where": {"public": true}}').should.be.true();
-        });
-    });
-
-    describe('fetch specific article', () => {
-
-        beforeEach(() => {
-            jsonSpy = spy();
-
-            class HttpService {
-                json(path: string) {
-                    jsonSpy(path);
-                    return Promise.resolve(ARTICLES[0]);
-                }
-            }
-
-            const {ArticlesService} = load('./articles.service', {
-                '../common/http.service': {
-                    HttpService
-                }
-            });
-
-            articlesService = new ArticlesService();
-
-        });
-
-        it('should fetch a specific article', async () => {
-            const response = await articlesService.fetchArticle('abc123');
-
-            response.should.eql(ARTICLES[0]);
-
-            jsonSpy.calledOnce.should.be.true();
-
-            jsonSpy.calledWith('/articles/abc123').should.be.true();
-        });
-    });
-
-    describe('save article', () => {
-
-        beforeEach(() => {
-            postSpy = spy();
-
-            class HttpService {
-                post(path: string, body: any, authorization: string) {
-                    postSpy(path, body, authorization);
-                    return 200;
-                }
-            }
-
-            const {ArticlesService} = load('./articles.service', {
-                '../common/http.service': {
-                    HttpService
-                }
-            });
-
-            articlesService = new ArticlesService();
-
-        });
-
-        it('should save a new article', async () => {
-            const response = await articlesService.saveArticle(ARTICLES[0]);
-
-            postSpy.calledOnce.should.be.true();
-
-            postSpy.calledWith('/articles', ARTICLES[0]).should.be.true();
-        });
-
-        it('should send authorization in header', async () => {
-            const response = await articlesService.saveArticle(ARTICLES[0], 'abc123');
-
-            postSpy.calledOnce.should.be.true();
-
-            postSpy.calledWith('/articles', ARTICLES[0], { authorization: 'abc123' }).should.be.true();
-        });
-    });
-
-    describe('edit article', () => {
-        beforeEach(() => {
-            putSpy = spy();
-
-            class HttpService {
-                put(path: string, body: any, authorization: any) {
-                    putSpy(path, body, authorization);
-                    return 200;
-                }
-            }
-
-            const {ArticlesService} = load('./articles.service', {
-                '../common/http.service': {
-                    HttpService
-                }
-            });
-
-            articlesService = new ArticlesService();
-        });
-
-        it('should edit an article', async () => {
-            const response = await articlesService.editArticle(ARTICLES[0]);
-
-            putSpy.calledOnce.should.be.true();
-
-            putSpy.calledWith(`/articles/${ARTICLES[0].id}`, ARTICLES[0]).should.be.true();
-        });
-
-        it('should send authorization in header', async () => {
-            const response = await articlesService.editArticle(ARTICLES[0], 'abc123');
-
-            putSpy.calledOnce.should.be.true();
-
-            putSpy.calledWith(`/articles/${ARTICLES[0].id}`, ARTICLES[0], { authorization: 'abc123' }).should.be.true();
-        });
-    });
-
-    describe('fetch preview', () => {
-        beforeEach(() => {
-            postSpy = spy();
-
-            class HttpService {
-                post(path: string, body: any, authorization: any) {
-                    postSpy(path, body, authorization);
-                    return 200;
-                }
-            }
-
-            const {ArticlesService} = load('./articles.service', {
-                '../common/http.service': {
-                    HttpService
-                }
-            });
-
-            articlesService = new ArticlesService();
-        });
-
-        it('should get a preview of markdown', async () => {
-            const response = await articlesService.fetchPreview('abc123', 'abc123');
-
-            putSpy.calledOnce.should.be.true();
-
-            postSpy.calledWith(`/articles/markdown`, { markdown: 'abc123' }, { authorization: 'abc123' }).should.be.true();
-        });
+      articlesService = new ArticlesService();
 
     });
 
-    describe('delete article', () => {
+    it('should fetch a list of articles from the server', async () => {
+      const response = await articlesService.fetchArticles();
 
-        beforeEach(() => {
-            deleteSpy = spy();
+      const filter = {
+        where: {
+          public: true
+        },
+        fields: {
+          content: false,
+          parsedContent: false
+        }
+      };
 
-            class HttpService {
-                delete(path: string, authorization: any) {
-                    deleteSpy(path, authorization);
-                    return 200;
-                }
-            }
+      response.should.eql(ARTICLES);
 
-            const {ArticlesService} = load('./articles.service', {
-                '../common/http.service': {
-                    HttpService
-                }
-            });
+      jsonSpy.calledOnce.should.be.true();
 
-            articlesService = new ArticlesService();
-        });
+      jsonSpy.calledWith(`/articles?filter=${JSON.stringify(filter)}`).should.be.true();
+    });
+  });
 
-        it('should delete an article', async () => {
-            const response = await articlesService.deleteArticle(ARTICLES[0], 'abc123');
+  describe('fetch specific article', () => {
 
-            deleteSpy.calledOnce.should.be.true();
+    beforeEach(() => {
+      jsonSpy = spy();
 
-            deleteSpy.calledWith(`/articles/${ARTICLES[0].id}`, { authorization: 'abc123' }).should.be.true();
-        });
+      class HttpService {
+        json(path: string) {
+          jsonSpy(path);
+          return Promise.resolve(ARTICLES[0]);
+        }
+      }
+
+      const {ArticlesService} = load('./articles.service', {
+        '../common/http.service': {
+          HttpService
+        }
+      });
+
+      articlesService = new ArticlesService();
 
     });
 
-    describe('fetch pending articles', () => {
+    it('should fetch a specific article', async () => {
+      const response = await articlesService.fetchArticle('abc123');
 
-        beforeEach(() => {
-            jsonSpy = spy();
+      response.should.eql(ARTICLES[0]);
 
-            class HttpService {
-                json(path: string) {
-                    jsonSpy(path);
-                    return Promise.resolve(ARTICLES);
-                }
-            }
+      jsonSpy.calledOnce.should.be.true();
 
-            const {ArticlesService} = load('./articles.service', {
-                '../common/http.service': {
-                    HttpService
-                }
-            });
-
-            articlesService = new ArticlesService();
-
-        });
-
-        it('should fetch a list of pending articles from the server', async () => {
-            const response = await articlesService.fetchPendingArticles();
-
-            response.should.eql(ARTICLES);
-
-            jsonSpy.calledOnce.should.be.true();
-
-            jsonSpy.calledWith('/articles?filter={"where": {"public": false}}').should.be.true();
-        });
+      jsonSpy.calledWith('/articles/abc123').should.be.true();
     });
+  });
+
+  describe('save article', () => {
+
+    beforeEach(() => {
+      postSpy = spy();
+
+      class HttpService {
+        post(path: string, body: any, authorization: string) {
+          postSpy(path, body, authorization);
+          return 200;
+        }
+      }
+
+      const {ArticlesService} = load('./articles.service', {
+        '../common/http.service': {
+          HttpService
+        }
+      });
+
+      articlesService = new ArticlesService();
+
+    });
+
+    it('should save a new article', async () => {
+      const response = await articlesService.saveArticle(ARTICLES[0]);
+
+      postSpy.calledOnce.should.be.true();
+
+      postSpy.calledWith('/articles', ARTICLES[0]).should.be.true();
+    });
+
+    it('should send authorization in header', async () => {
+      const response = await articlesService.saveArticle(ARTICLES[0], 'abc123');
+
+      postSpy.calledOnce.should.be.true();
+
+      postSpy.calledWith('/articles', ARTICLES[0], { authorization: 'abc123' }).should.be.true();
+    });
+  });
+
+  describe('edit article', () => {
+    beforeEach(() => {
+      putSpy = spy();
+
+      class HttpService {
+        put(path: string, body: any, authorization: any) {
+          putSpy(path, body, authorization);
+          return 200;
+        }
+      }
+
+      const {ArticlesService} = load('./articles.service', {
+        '../common/http.service': {
+          HttpService
+        }
+      });
+
+      articlesService = new ArticlesService();
+    });
+
+    it('should edit an article', async () => {
+      const response = await articlesService.editArticle(ARTICLES[0]);
+
+      putSpy.calledOnce.should.be.true();
+
+      putSpy.calledWith(`/articles/${ARTICLES[0].id}`, ARTICLES[0]).should.be.true();
+    });
+
+    it('should send authorization in header', async () => {
+      const response = await articlesService.editArticle(ARTICLES[0], 'abc123');
+
+      putSpy.calledOnce.should.be.true();
+
+      putSpy.calledWith(`/articles/${ARTICLES[0].id}`, ARTICLES[0], { authorization: 'abc123' }).should.be.true();
+    });
+  });
+
+  describe('fetch preview', () => {
+    beforeEach(() => {
+      postSpy = spy();
+
+      class HttpService {
+        post(path: string, body: any, authorization: any) {
+          postSpy(path, body, authorization);
+          return 200;
+        }
+      }
+
+      const {ArticlesService} = load('./articles.service', {
+        '../common/http.service': {
+          HttpService
+        }
+      });
+
+      articlesService = new ArticlesService();
+    });
+
+    it('should get a preview of markdown', async () => {
+      const response = await articlesService.fetchPreview('abc123', 'abc123');
+
+      putSpy.calledOnce.should.be.true();
+
+      postSpy.calledWith(`/articles/markdown`, { markdown: 'abc123' }, { authorization: 'abc123' }).should.be.true();
+    });
+
+  });
+
+  describe('delete article', () => {
+
+    beforeEach(() => {
+      deleteSpy = spy();
+
+      class HttpService {
+        delete(path: string, authorization: any) {
+          deleteSpy(path, authorization);
+          return 200;
+        }
+      }
+
+      const {ArticlesService} = load('./articles.service', {
+        '../common/http.service': {
+          HttpService
+        }
+      });
+
+      articlesService = new ArticlesService();
+    });
+
+    it('should delete an article', async () => {
+      await articlesService.deleteArticle(ARTICLES[0], 'abc123');
+
+      deleteSpy.calledOnce.should.be.true();
+
+      deleteSpy.calledWith(`/articles/${ARTICLES[0].id}`, { authorization: 'abc123' }).should.be.true();
+    });
+
+  });
+
+  describe('fetch pending articles', () => {
+
+    beforeEach(() => {
+      jsonSpy = spy();
+
+      class HttpService {
+        json(path: string) {
+          jsonSpy(path);
+          return Promise.resolve(ARTICLES);
+        }
+      }
+
+      const {ArticlesService} = load('./articles.service', {
+        '../common/http.service': {
+          HttpService
+        }
+      });
+
+      articlesService = new ArticlesService();
+
+    });
+
+    it('should fetch a list of pending articles from the server', async () => {
+      const response = await articlesService.fetchPendingArticles();
+
+      const filter = {
+        where: {
+          public: false
+        },
+        fields: {
+          content: false,
+          parsedContent: false
+        }
+      };
+
+      response.should.eql(ARTICLES);
+
+      jsonSpy.calledOnce.should.be.true();
+
+      jsonSpy.calledWith(`/articles?filter=${JSON.stringify(filter)}`).should.be.true();
+    });
+  });
 });
--- app/articles/articles.service.ts
+++ app/articles/articles.service.ts
@@ -1,41 +1,59 @@
 import 'whatwg-fetch';
-import { Auth } from '../admin/admin.state';
-import { HttpService } from '../common/http.service';
-import { Article } from '../landing/landing.state';
+import {Auth} from '../admin/admin.state';
+import {HttpService} from '../common/http.service';
+import {Article} from '../landing/landing.state';
 
 export class ArticlesService {
 
-    private httpService: HttpService;
+  private httpService: HttpService;
 
-    constructor() {
-        this.httpService = new HttpService();
-    }
+  constructor() {
+    this.httpService = new HttpService();
+  }
 
-    fetchArticles(): Promise<Article[]> {
-        return this.httpService.json<Article[]>('/articles?filter={"where": {"public": true}}');
-    }
+  fetchArticles(): Promise<Article[]> {
+    const filter = {
+      where: {
+        public: true
+      },
+      fields: {
+        content: false,
+        parsedContent: false
+      }
+    };
+    return this.httpService.json<Article[]>(`/articles?filter=${JSON.stringify(filter)}`);
+  }
 
-    fetchArticle(articleId: string): Promise<Article> {
-        return this.httpService.json<Article>(`/articles/${articleId}`);
-    }
+  fetchArticle(articleId: string): Promise<Article> {
+    return this.httpService.json<Article>(`/articles/${articleId}`);
+  }
 
-    saveArticle(article: Article, authorization: string): Promise<Article> {
-        return this.httpService.post<Article>('/articles', article, { authorization });
-    }
+  saveArticle(article: Article, authorization: string): Promise<Article> {
+    return this.httpService.post<Article>('/articles', article, {authorization});
+  }
 
-    editArticle(article: Article, authorization: string): Promise<Article> {
-        return this.httpService.put<Article>(`/articles/${article.id}`, article, { authorization });
-    }
+  editArticle(article: Article, authorization: string): Promise<Article> {
+    return this.httpService.put<Article>(`/articles/${article.id}`, article, {authorization});
+  }
 
-    fetchPreview(markdown: string, authorization: string): Promise<string> {
-        return this.httpService.post<string>('/articles/markdown', { markdown }, { authorization });
-    }
+  fetchPreview(markdown: string, authorization: string): Promise<string> {
+    return this.httpService.post<string>('/articles/markdown', {markdown}, {authorization});
+  }
 
-    deleteArticle(article: Article, authorization: string): Promise<Number> {
-        return this.httpService.delete(`/articles/${article.id}`, { authorization });
-    }
+  deleteArticle(article: Article, authorization: string): Promise<Number> {
+    return this.httpService.delete(`/articles/${article.id}`, {authorization});
+  }
 
-    fetchPendingArticles(): Promise<Article[]> {
-        return this.httpService.json<Article[]>('/articles?filter={"where": {"public": false}}');
-    }
+  fetchPendingArticles(): Promise<Article[]> {
+    const filter = {
+      where: {
+        public: false
+      },
+      fields: {
+        content: false,
+        parsedContent: false
+      }
+    };
+    return this.httpService.json<Article[]>(`/articles?filter=${JSON.stringify(filter)}`);
+  }
 }
